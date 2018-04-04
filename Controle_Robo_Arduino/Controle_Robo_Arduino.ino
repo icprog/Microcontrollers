0a61fbfddedbc98fac031cc179c4a76051381dfe
Will Douglas <willdowglas@gmail.com>
2018-04-04 19:46:50 -0300

    fixed bad ident

--- Controle_Robo_Arduino/Controle_Robo_Arduino.ino
+++ Controle_Robo_Arduino/Controle_Robo_Arduino.ino
@@ -8,127 +8,130 @@ const int ponteH01DIR = 10, ponteH01ESQ = 9,
 bool a = 0, b = 0;
 char c, d;
 
-void zero() 
-{
-  digitalWrite(ponteH01DIR, LOW);
-  digitalWrite(ponteH02DIR, LOW);
-  digitalWrite(ponteH01ESQ, LOW);
-  digitalWrite(ponteH02ESQ, LOW);
+void zero(){
+    digitalWrite(ponteH01DIR, LOW);
+    digitalWrite(ponteH02DIR, LOW);
+    digitalWrite(ponteH01ESQ, LOW);
+    digitalWrite(ponteH02ESQ, LOW);
 }
 
-void up()
-{
-  zero();
-  digitalWrite(ponteH01DIR, HIGH);
-  digitalWrite(ponteH02DIR, HIGH);
+void up(){
+    zero();
+    
+    digitalWrite(ponteH01DIR, HIGH);
+    digitalWrite(ponteH02DIR, HIGH);
 }
-void down()
-{
-  zero();
-  digitalWrite(ponteH01ESQ, HIGH);
-  digitalWrite(ponteH02ESQ, HIGH);
+
+void down(){
+    zero();
+    
+    digitalWrite(ponteH01ESQ, HIGH);
+    digitalWrite(ponteH02ESQ, HIGH);
 }
-void left()
-{
-  zero();
-  analogWrite(ponteH01ESQ, pwm);
-  analogWrite(ponteH02DIR, pwm);
+
+void left(){
+    zero();
+    
+    analogWrite(ponteH01ESQ, pwm);
+    analogWrite(ponteH02DIR, pwm);
 }
-void righ()
-{
-  zero();
-  analogWrite(ponteH01DIR, pwm);
-  analogWrite(ponteH02ESQ, pwm);
+
+void righ(){
+    zero();
+    
+    analogWrite(ponteH01DIR, pwm);
+    analogWrite(ponteH02ESQ, pwm);
 }
-void weaponX()
-{ 
-    if(a == 0 && b == 0)
-    {
-      digitalWrite(ponteH03ESQ,LOW);
-      digitalWrite(ponteH03DIR,HIGH);
-      a = 1;
+
+void weaponX(){ 
+    if(a == 0 && b == 0){
+        digitalWrite(ponteH03ESQ,LOW);
+        digitalWrite(ponteH03DIR,HIGH);
+        
+        a = 1;
     }
 }
-void weaponXoff()
-{ 
+
+void weaponXoff(){ 
     digitalWrite(ponteH03ESQ,LOW);
     digitalWrite(ponteH03DIR,LOW);
+    
     a = 0;
+    
     delay(30);
 }
-void weaponSquare()
-{
-    if(a == 0 && b == 0)
-    {
-      digitalWrite(ponteH03DIR,LOW);
-      digitalWrite(ponteH03ESQ,HIGH);
-      b = 1;
+
+void weaponSquare(){
+    if(a == 0 && b == 0){
+        digitalWrite(ponteH03DIR,LOW);
+        digitalWrite(ponteH03ESQ,HIGH);
+        
+        b = 1;
     }
 }
-void weaponSquareoff()
-{
+void weaponSquareoff(){
     digitalWrite(ponteH03DIR,LOW);
     digitalWrite(ponteH03ESQ,LOW);
+    
     b = 0;
+    
     delay(30);
 }
 
-void setup()
-{
-  pinMode(ponteH01DIR, OUTPUT);
-  pinMode(ponteH01ESQ, OUTPUT);
-  pinMode(ponteH02DIR, OUTPUT);
-  pinMode(ponteH02ESQ, OUTPUT);
-  pinMode(ponteH03DIR, OUTPUT);
-  pinMode(ponteH03ESQ, OUTPUT);
-  d = 'S';
-  c = 'S';
+void setup(){
+    pinMode(ponteH01DIR, OUTPUT);
+    pinMode(ponteH01ESQ, OUTPUT);
+    pinMode(ponteH02DIR, OUTPUT);
+    pinMode(ponteH02ESQ, OUTPUT);
+    pinMode(ponteH03DIR, OUTPUT);
+    pinMode(ponteH03ESQ, OUTPUT);
+    
+    d = 'S';
+    c = 'S';
 }
 
-
-void loop()
-{
-  while (mySerial.available())
-  {
-    d = c;
-    c = mySerial.read();
-    if (c != d)
+void loop(){
+    while (mySerial.available())
     {
-      zero();
-      delay(30);
+        d = c;
+        c = mySerial.read();
+        
+        if (c != d){
+            zero();
+            delay(30);
+        }
+        
+        switch(c){
+            case 'F':
+                up();
+                break;
+            case 'B':
+                down();
+                break;
+            case 'L':
+                left();
+                break;
+            case 'R':
+                righ();
+                break;
+            case 'S':
+                zero();
+                break;
+            case 'W':
+                weaponX();
+                break; 
+            case 'w':
+                weaponXoff();
+                break;
+            case 'U':
+                weaponSquare();
+                break;
+            case 'u':
+                weaponSquareoff();
+                break;
+            default:
+                zero();
+                break;
+        }
     }
-      switch(c)
-      {
-        case 'F':
-          up();
-          break;
-        case 'B':
-          down();
-          break;
-        case 'L':
-          left();
-          break;
-        case 'R':
-          righ();
-          break;
-        case 'S':
-          zero();
-          break;
-        case 'W':
-          weaponX();
-          break; 
-        case 'w':
-          weaponXoff();
-          break;
-        case 'U':
-          weaponSquare();
-          break;
-        case 'u':
-          weaponSquareoff();
-          break;
-        default:
-          zero();
-          break;
-      }
-  }
 }
